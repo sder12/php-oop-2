451a18e662ed6db672823bd3b8873950b21bc61e
sder12 <sara.cetto@gmail.com>
2022-12-13 16:42:18 +0100

    Some corrections

--- index.php
+++ index.php
@@ -5,15 +5,14 @@
 // require_once __DIR__ . "/Models/Food.php";
 // require_once __DIR__ . "/Models/Game.php";
 // require_once __DIR__ . "/Models/Sleep.php";
-//Classi user
-require_once __DIR__ . "/Models/User.php";
+
 
 
 // Databse con istanze
 require_once __DIR__ . "/Database/database.php";
 
 //Html 
-require_once __DiR__ . "./Views/Layout/head.php";
+require_once __DIR__ . "./Views/Layout/head.php";
 require_once __DIR__ . "./Views/Partials/header.php";
 require_once __DIR__ . "./Views/Partials/main.php";
 require_once __DIR__ . "./Views/Layout/footer.php";
